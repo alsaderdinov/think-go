4266b826b20d10fc22d7eefc15fbfb0230423940
Alexander Saderdinov <al.saderdinov@gmail.com>
2023-06-29 10:48:42 +0300

    base implementation of search engine

---
+++ search/cmd/search/search.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"strings"
+
+	"think-go/search/pkg/crawler"
+	"think-go/search/pkg/crawler/spider"
+)
+
+const depth = 2
+
+var urls = []string{"https://go.dev", "https://practical-go-lessons.com"}
+
+func main() {
+	query := flag.String("s", "", "query to find URLs")
+	flag.Parse()
+
+	if *query == "" {
+		fmt.Println("The query is empty. Please, read the help: -help")
+		return
+	}
+
+	docs, err := scan(urls, depth)
+	if err != nil {
+		log.Fatal("Error scanning documents:", err)
+	}
+
+	res := search(docs, *query)
+
+	for _, i := range res {
+		fmt.Printf("%s: %s\n", i.Title, i.URL)
+	}
+}
+
+// scan осуществляет обход ссылок сайта, указанного в URL,
+// с учётом глубины перехода по ссылкам, переданной в depth.
+func scan(urls []string, depth int) ([]crawler.Document, error) {
+	var res []crawler.Document
+
+	s := spider.New()
+
+	for _, url := range urls {
+		docs, err := s.Scan(url, depth)
+		if err != nil {
+			log.Println("Error scanning URL:", url, "Error:", err)
+			continue
+		}
+
+		res = append(res, docs...)
+	}
+
+	return res, nil
+}
+
+// search ищет ссылки в переданном списке документов по заданному запросу и возвращает совпадающие документы.
+// выполняет поиск без учета регистра, сравнивая запрос переданный в query с URL-адресами документов
+func search(docs []crawler.Document, query string) []crawler.Document {
+	var res []crawler.Document
+
+	for _, doc := range docs {
+		if strings.Contains(strings.ToLower(doc.URL), strings.ToLower(query)) {
+			res = append(res, doc)
+		}
+	}
+	return res
+}
