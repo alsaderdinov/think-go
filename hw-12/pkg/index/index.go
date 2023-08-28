42b357b8c545c6e670ba914380a97f5e4fe39387
Alexander Saderdinov <al.saderdinov@gmail.com>
2023-08-29 01:25:57 +0300

    simple webapp

---
+++ hw-12/pkg/index/index.go
@@ -0,0 +1,41 @@
+// Package index предоставляет простую службу индексации
+// целочисленные значения, связанные с конкретными словами.
+package index
+
+import (
+	"strings"
+)
+
+// Service - служба индексации.
+type Service struct {
+	data map[string][]int
+}
+
+// New конструктор службы индексации.
+func New() *Service {
+	return &Service{
+		data: make(map[string][]int),
+	}
+}
+
+// Add добавляет заданное целочисленное значение к индексу для каждого слова в предоставленной строке.
+// Разбивает входную строку на слова, преобразует их в строчные и связывает
+// заданное целочисленное значение с каждым словом во внутреннем хранилище данных.
+func (svc *Service) Add(s string, n int) {
+	words := strings.Fields(strings.ToLower(s))
+	for _, w := range words {
+		svc.data[w] = append(svc.data[w], n)
+	}
+}
+
+// Find извлекает целочисленные значения, связанные с заданным словом.
+// Возвращается фрагмент, содержащий все целочисленные значения, связанные с данным словом.
+// Если слово не найдено в индексе, возвращается пустой фрагмент.
+func (svc *Service) Find(s string) []int {
+	return svc.data[strings.ToLower(s)]
+}
+
+// Data геттер
+func (svc *Service) Data() map[string][]int {
+	return svc.data
+}
---
+++ hw-12/pkg/webapp/webapp.go
@@ -0,0 +1,45 @@
+package webapp
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"think-go/hw-12/pkg/crawler"
+	"think-go/hw-12/pkg/index"
+
+	"github.com/gorilla/mux"
+)
+
+const addr = "localhost:8080"
+
+type Service struct {
+	idx  *index.Service
+	docs []crawler.Document
+}
+
+func New(idx *index.Service, docs []crawler.Document) *Service {
+	return &Service{
+		idx:  idx,
+		docs: docs,
+	}
+}
+
+func (s *Service) Start() {
+	r := mux.NewRouter()
+	r.HandleFunc("/docs", s.docsHandler).Methods(http.MethodGet)
+	r.HandleFunc("/index", s.indexHandler).Methods(http.MethodGet)
+
+	log.Fatal(http.ListenAndServe(addr, r))
+}
+
+func (s *Service) docsHandler(w http.ResponseWriter, _ *http.Request) {
+	if err := json.NewEncoder(w).Encode(s.docs); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func (s *Service) indexHandler(w http.ResponseWriter, _ *http.Request) {
+	if err := json.NewEncoder(w).Encode(s.idx.Data()); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
